21b935cf94ed02f05dda6d6deb8b73a49157e978
Asyraf J <asyraf@rocketbots.io>
2021-12-30 14:29:54 +0800

    Chore: Removed console.log

--- routes/protectedRoutes.js
+++ routes/protectedRoutes.js
@@ -5,7 +5,6 @@ const {check} = require('express-validator');
 module.exports = (_, express) => {
   const router = new express.Router({strict: true});
   const channelId = process.env.CHANNEL_ID;
-    console.log('haha')
   router.post('/message-id', [
     check('message').isArray().withMessage('Message must be an array.'),
     check('channelId').not().isEmpty().withMessage('Your channelId is required.').equals(channelId),
