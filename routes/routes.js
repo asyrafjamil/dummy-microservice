f389264abc04faa9497ee669058494f2a062d71f
Asyraf J <asyraf@rocketbots.io>
2022-01-04 17:36:23 +0800

    Chore: Eslint fixed

--- routes/routes.js
+++ routes/routes.js
@@ -1,7 +1,7 @@
-const publicRoutes = require('./publicRoutes')
-const protectedRoutes = require('./protectedRoutes')
+const publicRoutes = require('./publicRoutes');
+const protectedRoutes = require('./protectedRoutes');
 
 module.exports = (app, express) => {
-    app.use('/', publicRoutes(app, express));
-    app.use('/', protectedRoutes(app, express));
-  };
+  app.use('/', publicRoutes(app, express));
+  app.use('/', protectedRoutes(app, express));
+};
