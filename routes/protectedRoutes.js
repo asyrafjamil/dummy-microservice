59286331e0ca70e99648cd9694ab2b8d0587cbe3
Asyraf J <asyraf@rocketbots.io>
2022-01-04 17:42:49 +0800

    Feature: Validate token for all protected routes

--- routes/protectedRoutes.js
+++ routes/protectedRoutes.js
@@ -6,11 +6,13 @@ module.exports = (_, express) => {
   const router = new express.Router({strict: true});
   const channelId = process.env.CHANNEL_ID;
 
+  router.use(validator.validateToken);
+
   router.post('/message', [
     check('message').isObject().withMessage('Message must be an object.'),
     check('channelId').not().isEmpty().withMessage('Your channelId is required.').equals(channelId),
     check('contactId').not().isEmpty().withMessage('Your contactId is required.'),
-  ], validator.validateToken, validator.validateRequestBody, sendMessageController);
+  ], validator.validateRequestBody, sendMessageController);
 
   return router;
 };
