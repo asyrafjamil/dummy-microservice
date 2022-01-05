3555221e52419512403fa6aee7ec730c1529e074
Leo <leoawesome21@gmail.com>
2022-01-05 11:49:51 +0800

    Update error payload

--- routes/index.js
+++ routes/index.js
@@ -8,42 +8,35 @@ router.get('/', function(req, res) {
 });
 
 router.post('/message', (req, res)=> {
-  try {
-    // Get the brearer token from request header.
-    const bearerToken = req.headers.authorization;
-    if (!bearerToken) {
-      return res.status(404).send({
-        status: 'bearer_token_not_found',
-        message: 'Bearer token is not found.',
-      });
-    }
-
-    // Compare the token from header with the token from environment variable.
-    const token = bearerToken.substring(7, bearerToken.length);
-    if (process.env.CHANNEL_TOKEN !== token) {
-      return res.status(401).send({
-        status: 'unathorized_user',
-        message: 'User is unathorized.',
-      });
-    }
-
-    /**
-     * User should retrieve the message from the req.body,
-     * transform the message and send it out,
-     * return the mId (ID of sent message) and success status on success call,
-     * else return the error message and failed status.
-     */
-
-    res.status(200).send({
-      status: 'success',
-      mId: nanoid(),
-    });
-  } catch (err) {
-    res.status(500).send({
-      status: 'failed',
-      message: 'Something is wrong.',
+  // Get the brearer token from request header.
+  const bearerToken = req.headers.authorization;
+  if (!bearerToken) {
+    return res.status(404).send({
+      'error': {
+        'message': 'Token is not found!',
+      },
     });
   }
+
+  // Compare the token from header with the token from environment variable.
+  const token = bearerToken.substring(7, bearerToken.length);
+  if (process.env.CHANNEL_TOKEN !== token) {
+    return res.status(401).send({
+      'error': {
+        'message': 'Unauthorized token!',
+      },
+    });
+  }
+
+  /**
+   * User should retrieve the message from the req.body,
+   * transform the message and send it out,
+   * return the mId (ID of sent message).
+   */
+
+  res.status(200).send({
+    mId: nanoid(),
+  });
 });
 
 module.exports = router;
