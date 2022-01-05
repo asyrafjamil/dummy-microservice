a7ed6d735fe721aafb35a4195b364c30a932f86b
Asyraf J <asyraf@rocketbots.io>
2022-01-05 11:15:18 +0800

    Chore: Assigned all the functions inside of routes.js

--- routes/routes.js
+++ routes/routes.js
@@ -1,7 +1,40 @@
-const publicRoutes = require('./publicRoutes');
-const protectedRoutes = require('./protectedRoutes');
-
+const {nanoid} = require('nanoid');
 module.exports = (app, express) => {
-  app.use('/', publicRoutes(app, express));
-  app.use('/', protectedRoutes(app, express));
+  app.use('/', route(app, express));
+};
+
+const route = (_, express) => {
+  const router = new express.Router({strict: true});
+
+  router.use(validateToken);
+
+  router.post('/message', (req, res)=> {
+    const mId = nanoid();
+
+    res.status(200).send({
+      status: 'success',
+      mId,
+    });
+  });
+
+  return router;
+};
+
+const validateToken = (req, res, next) =>{
+  const bearerToken = req.headers.authorization;
+  if (!bearerToken) {
+    return res.status(404).send({
+      status: 'bearer_token_not_found',
+      message: 'Bearer token is not found.',
+    });
+  }
+  const token = bearerToken.substring(7, bearerToken.length);
+
+  if (process.env.CHANNEL_TOKEN !== token) {
+    return res.status(401).send({
+      status: 'unathorized_user',
+      message: 'User is unathorized.',
+    });
+  }
+  next();
 };
