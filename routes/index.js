fece529939ea84c4d00d08cb0840acf95e3ab0a0
Asyraf J <asyraf@rocketbots.io>
2022-01-06 11:09:22 +0800

    Docs: Updated jsDoc in route file

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,8 @@
+/**
+ * Inside this file, the route is placed between the Custom Channel and 3rd Party Chat application.
+ * The process happens here.
+ */
+
 const {nanoid} = require('nanoid');
 const express = require('express');
 // eslint-disable-next-line new-cap
@@ -11,27 +16,21 @@ router.post('/message', (req, res)=> {
   // Get the bearer token from request header.
   const bearerToken = req.headers.authorization;
   if (!bearerToken) {
-    return res.status(404).send({
-      'error': {
-        'message': 'Token is not found!',
-      },
-    });
+    return res.status(401);
   }
 
   // Compare the token from header with the token from environment variable.
   const token = bearerToken.substring(7, bearerToken.length);
   if (process.env.CHANNEL_TOKEN !== token) {
-    return res.status(401).send({
-      'error': {
-        'message': 'Unauthorized token!',
-      },
-    });
+    return res.status(401);
   }
 
   /**
    * User should retrieve the message from the req.body,
    * transform the message and send it out,
    * return the mId (ID of sent message).
+   * Output example:
+   * "mId": "_CX_faR8Ro_yTzhf4AtI3"
    */
 
   res.status(200).send({
